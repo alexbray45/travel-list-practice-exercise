47b3e8a16225912debb76188ed7e48eaebfa8622
Alexander Bray <164928593+alexbray45@users.noreply.github.com>
2025-05-01 11:10:55 +0200

    update-v2-1

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,10 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do need for your trip?🤔</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
